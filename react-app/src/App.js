cc757ecaba21558b10943e3401dfe70cc96eef34
Kai <kaibing.c@gmail.com>
2020-08-09 14:44:59 +0800

    create dev env

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Banner from "./components/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Banner component */}
+      <Banner />
+      In App.js
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/ItemInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ItemInfo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Item Information</h1>
+      </div>
+    );
+  }
+}
+
+export default ItemInfo;
